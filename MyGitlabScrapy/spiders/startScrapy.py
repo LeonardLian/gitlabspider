73077693f95a6560d4a88f306c6688166529ee8e
soilerl <1062734409@qq.com>
2019-04-24 17:36:43 +0800

    添加了四个数组，里面的值就是各位需要爬取的数据的网址，array_lbh刘博涵，array_lwg刘文港，array_skw宋凯文,array_ld练达，各25个

--- MyGitlabScrapy/spiders/startScrapy.py
+++ MyGitlabScrapy/spiders/startScrapy.py
@@ -7,7 +7,8 @@ array_skw = ['https://gitlab.com/Isleward/isleward','https://gitlab.com/muttmua/
 array_ld = ['https://gitlab.com/postmarketOS/pmbootstrap','https://gitlab.com/openconnect/ocserv','https://gitlab.com/tom79/mastalab','https://gitlab.com/Mr_Goldberg/goldberg_emulator','https://gitlab.com/sleepyhead/sleepyhead-code','https://gitlab.com/fatihacet/gitlab-vscode-extension','https://gitlab.com/mojo42/Jirafeau','https://gitlab.com/painlessMesh/painlessMesh','https://gitlab.com/flectra-hq/flectra','https://gitlab.com/Mactroll/NoMAD','https://gitlab.com/git-latexdiff/git-latexdiff','https://gitlab.com/gitlab-org/gitlab-shell','https://gitlab.com/gitlab-org/gitlab-runner-docker-cleanup','https://gitlab.com/sortix/sortix','https://gitlab.com/gableroux/unity3d-gitlab-ci-example','https://gitlab.com/cunidev/gestures','https://gitlab.com/sequoia-pgp/sequoia','https://gitlab.com/postmill/Postmill','https://gitlab.com/pdfgrep/pdfgrep','https://gitlab.com/dessalines/torrents.csv','https://gitlab.com/angelkyo/w10-digitallicense','https://gitlab.com/MasterPassword/MasterPassword','https://gitlab.com/NebulousLabs/Sia','https://gitlab.com/procps-ng/procps','https://gitlab.com/eidheim/Simple-Web-Server']
 
 def startScrapy():
-    baseURL = "https://gitlab.com/fdroid/fdroidclient/commits/master";
+    #baseURL = "https://gitlab.com/fdroid/fdroidclient/commits/master";
+
     project_commit_str = "scrapy crawl project_commit -a baseURL="+baseURL+" -o project_commit.json -s LOG_FILE=project_commit.log"
     print project_commit_str
     cmdline.execute(project_commit_str.split())
