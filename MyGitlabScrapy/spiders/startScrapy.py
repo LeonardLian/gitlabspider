e87f6711a4c6cf99e4deabd62f80f2de29dc5fc4
161250066 <1147588658@qq.com>
2019-04-24 18:33:59 +0800

    normal distribution

--- MyGitlabScrapy/spiders/startScrapy.py
+++ MyGitlabScrapy/spiders/startScrapy.py
@@ -18,32 +18,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-str_ld = [
-          'https://gitlab.com/postmarketOS/pmbootstrap',
-          'https://gitlab.com/openconnect/ocserv',
-          'https://gitlab.com/tom79/mastalab',
-          'https://gitlab.com/Mr_Goldberg/goldberg_emulator',
-          'https://gitlab.com/sleepyhead/sleepyhead-code',
-          'https://gitlab.com/fatihacet/gitlab-vscode-extension',
-          'https://gitlab.com/mojo42/Jirafeau',
-          'https://gitlab.com/painlessMesh/painlessMesh',
-          'https://gitlab.com/flectra-hq/flectra',
-          'https://gitlab.com/Mactroll/NoMAD',
-          'https://gitlab.com/git-latexdiff/git-latexdiff',
-          'https://gitlab.com/gitlab-org/gitlab-shell',
-          'https://gitlab.com/gitlab-org/gitlab-runner-docker-cleanup',
-          'https://gitlab.com/sortix/sortix',
-          'https://gitlab.com/gableroux/unity3d-gitlab-ci-example',
-          'https://gitlab.com/cunidev/gestures',
-          'https://gitlab.com/sequoia-pgp/sequoia',
-          'https://gitlab.com/postmill/Postmill',
-          'https://gitlab.com/pdfgrep/pdfgrep',
-          'https://gitlab.com/dessalines/torrents.csv',
-          'https://gitlab.com/angelkyo/w10-digitallicense',
-          'https://gitlab.com/MasterPassword/MasterPassword',
-          'https://gitlab.com/NebulousLabs/Sia',
-          'https://gitlab.com/procps-ng/procps',
-          'https://gitlab.com/eidheim/Simple-Web-Server']
