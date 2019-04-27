1e45d0c452821d0fb385b98889bd2db49aa41e17
161250066 <1147588658@qq.com>
2019-04-27 11:03:49 +0800

    commit

--- MyGitlabScrapy/spiders/commit_info.py
+++ MyGitlabScrapy/spiders/commit_info.py
@@ -1,6 +1,7 @@
 import scrapy
 from MyGitlabScrapy.items import CommitInfoItem
 import json
+import os
 
 class MySpider(scrapy.Spider):
     name = 'commit_info'
@@ -10,20 +11,35 @@ class MySpider(scrapy.Spider):
 
     def __init__(self, filePath=None, *args, **kwargs):
         super(MySpider, self).__init__(*args, **kwargs)
-        self.filePath = filePath
+        # self.filePath = filePath
 
     def start_requests(self):
         start_urls = []
-        file = open(self.filePath, "rb")
-        data = json.load(file)
-        for item in data:
-            start_urls.append(item["commit_href"])
+        # file = open(self.filePath, "rb")
+
+        files = os.listdir(os.getcwd())
+
+        for json_file in files:
+            if json_file.split(".")[1] == "json" and json_file.split(".")[0] != "project_commit":
+                print json_file
+                file_info = open(json_file, "rb")
+                data = json.load(file_info)
+                for item in data:
+                    print item["commit_href"]
+                    start_urls.append(item["commit_href"])
+
+            else:
+                pass
 
         for url in start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         commit_info = CommitInfoItem()
+
+        page = (response.url.split("/"))[3]+"_"+(response.url.split("/"))[4]
+
+        commit_info['project_name'] = page
 
         commit_info['commit_id'] = response.xpath( '//div[@class="header-main-content"]/button[@class="btn btn-clipboard btn-transparent"]/@data-clipboard-text').extract()
         commit_info['commit_time'] = response.xpath('//div[@class="header-main-content"]/time/@datetime').extract()
@@ -37,4 +53,5 @@ class MySpider(scrapy.Spider):
 
         commit_info['additions_num'] = response.xpath('//span[@class="diff-stats-additions-deletions-expanded"]/strong[@class="cgreen"]/text()').extract()
         commit_info['deletions_num'] = response.xpath('//span[@class="diff-stats-additions-deletions-expanded"]/strong[@class="cred"]/text()').extract()
+        print commit_info
         yield commit_info
